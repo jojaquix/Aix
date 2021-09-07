0770415c6684b255a398039d2f73aae3cc12fb8e
jhon james quintero <jojaquix@gmail.com>
2021-09-07 08:28:40 -0500

    Agregar archivos de proyecto.

---
+++ AixClient/AixClient.cpp
@@ -0,0 +1,12 @@
+﻿// AixClient.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "AixClient.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
